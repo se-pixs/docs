bfaaa2271d634548f0b69a6a8a94351a4bc3df75
Konti08 <fabian.albrecht08@gmail.com>
2021-11-04 16:59:05 +0100

    add: .feature for convert low poly

--- narratives/features/step_definitions/stepdefs.js
+++ narratives/features/step_definitions/stepdefs.js
@@ -5,13 +5,33 @@ Given('image is in format {word}', function (format) {
     this.format = format;
 });
 
+Given('images has successfully been converted to low poly art'), function () {
+    this.success = true;
+}
+
+Given('images has not successfully been converted to low poly art'), function () {
+    this.success = true;
+}
+
+When('user chooses action convert to low poly art'), function () {
+    this.actualAnswer = "";
+}
+
 When('user drags image in into input field', function () {
     this.actualAnswer = isItPNGOrJPEG(this.format);
 });
 
 Then('user should see a {string}', function (expectedAnswer) {
-    assert.strictEqual(this.actualAnswer, expectedAnswer);
+    assert.equal(this.actualAnswer, expectedAnswer);
 });
+
+function isSuccessful(success) {
+    if (success === true) {
+        return 'success message';
+    } else {
+        return 'warning message';
+    }
+}
 
 function isItPNGOrJPEG(format) {
     if (format === 'PNG' || format === 'JPEG') {
